d5c9f390136ff6fde087ea55337816313a2b82c8
ivanrovito <ivanrovito@gmail.com>
2014-05-26 16:01:28 -0400

    Modif desde laptop

--- ivan/upload/process.php
+++ ivan/upload/process.php
@@ -3,10 +3,18 @@ require_once("conexion.php");
 $foto="";
 $foto=$_FILES["foto"]["name"];
 $temp=$_FILES["foto"]["tmp_name"];
+$tamano=$_FILES["foto"]["size"];
+$tipo=$_FILES["foto"]["type"];
 if(isset($_POST['foto'])){ $notify = $_POST['foto']; } //arregla el Undefined Index
 echo "el archivo en el PC del cliente se llama: <strong>$foto</strong>";
 echo "<br>";
 echo "el archivo en el Servidor se llama <strong>$temp</strong>";
+echo "<br>";
+echo "el tamaño en bytes del archivo es <strong>$tamano</strong>";
+echo "<br>";
+echo "el tipo del archivo es <strong>$tipo<type>";
+echo "<hr>";
+
 
 
 /*
