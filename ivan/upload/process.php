212d5f22fc6c0983368439a5013b63e5ad989559
ivanrovito <ivanrovito@gmail.com>
2014-05-26 16:17:27 -0400

    upload modificado

--- ivan/upload/process.php
+++ ivan/upload/process.php
@@ -6,6 +6,24 @@ $temp=$_FILES["foto"]["tmp_name"];
 $tamano=$_FILES["foto"]["size"];
 $tipo=$_FILES["foto"]["type"];
 if(isset($_POST['foto'])){ $notify = $_POST['foto']; } //arregla el Undefined Index
+//**********************************************************************
+//Ahora validamos que el tamaño del archivo sea el que necesitamos
+
+$kilobytes=$tamano/1024;//cant en kb
+//$mega=$kilobytes/1024;
+if ($kilobytes > 300)
+{
+		?>		
+		El archivo subido supera los 300 Kb
+		<br />
+		<input type="button" value="Volver" title="Volver" onclick="history.back()" />
+		<?php		
+}else 
+{
+	echo "Todo ok";
+}		
+
+/*
 echo "el archivo en el PC del cliente se llama: <strong>$foto</strong>";
 echo "<br>";
 echo "el archivo en el Servidor se llama <strong>$temp</strong>";
@@ -14,7 +32,7 @@ echo "el tamaño en bytes del archivo es <strong>$tamano</strong>";
 echo "<br>";
 echo "el tipo del archivo es <strong>$tipo<type>";
 echo "<hr>";
-
+*/
 
 
 /*
