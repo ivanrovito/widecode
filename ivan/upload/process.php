c6f4e91cee80a9f60338d1390d4336d7e04c624b
ivanrovito <ivanrovito@gmail.com>
2014-05-26 14:14:19 -0400

    process.php

--- ivan/upload/process.php
+++ ivan/upload/process.php
@@ -4,9 +4,9 @@ $foto="";
 $foto=$_FILES["foto"]["name"];
 $temp=$_FILES["foto"]["tmp_name"];
 if(isset($_POST['foto'])){ $notify = $_POST['foto']; } //arregla el Undefined Index
-echo "el archivo en el PC se llama: $foto";
+echo "el archivo en el PC del cliente se llama: <strong>$foto</strong>";
 echo "<br>";
-echo "el archivo en el Servidor se llama $temp";
+echo "el archivo en el Servidor se llama <stron>$temp</strong>";
 
 
 /*
